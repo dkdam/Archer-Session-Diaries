7539a523c9cf99a189288bf51cff7bd162e176c6
dkdam <dkdam@live.com>
2022-07-25 20:28:18 +1000

    equipment and login running

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -2,6 +2,7 @@ class EquipmentsController < ApplicationController
   before_action :check_for_login
 
   def index
+    @equipments = Equipment.all
   end
 
   def new
@@ -9,21 +10,29 @@ class EquipmentsController < ApplicationController
   end
 
   def create
-    equipment = Equipment.create equipment_params
-    @current_user.equipment << equipment # Give the new equipment to the current user
-    redirect_to root_path # IRL redirect to equipment or somewhere
+    equipments = Equipment.create equipment_params
+    @current_user.equipments << equipments # Give the new equipment to the current user
+    redirect_to equipments_path # IRL redirect to equipment or somewhere
   end
 
   def edit
+    @equipment = Equipment.find params[:id]
   end
 
   def update
+    equipments = Equipment.find params[:id]
+    equipments.update equipment_params
+    redirect_to equipments_path
   end
 
   def show
+    @equipment = Equipment.find params[:id]
   end
 
   def destroy
+    equipments = Equipment.find params[:id]
+    equipments.destroy
+    redirect_to equipments_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:new, :create]
   resources :users, :only => [:new, :create, :index]
   
-
-  resources :equipments, :only => [:new, :create, :index]
+  resources :equipments
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
